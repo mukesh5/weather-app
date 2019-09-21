b17cf69bf0d60622bc4168f162bd8dd5d09487f8
Mukesh <mukeshchugh5@gmail.com>
2019-09-21 16:18:10 +0530

    hourly weather display added

--- weather-app/src/components/HourlyWeather.js
+++ weather-app/src/components/HourlyWeather.js
@@ -5,7 +5,11 @@ class HourlyWeather extends React.Component {
 	render() {
 		return(
 			<div>
-				<h2>Hourly Weather Component</h2>
+				{this.props.weather.dt_txt && <p> Date Time: {this.props.weather.dt_txt}</p>}
+				{this.props.weather.main && <p>Max Temperature: {this.props.weather.main.temp_max}</p>}
+				{this.props.weather.main && <p>Min Temperature: {this.props.weather.main.temp_min}</p>}
+				{this.props.weather.main && <p>Humidity: {this.props.weather.main.humidity}</p>}
+				{this.props.weather.weather && <p>{this.props.weather.weather[0].description}</p>}
 			</div>
 		);
 	}
--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -6,14 +6,14 @@ class Weather extends React.Component {
 	render() {
 		return(
 			<div>
-				{this.props.responseData && <HourlyWeather/>}
-				{this.props.responseData && <HourlyWeather/>}
-				{this.props.responseData && <HourlyWeather/>}
-				{this.props.responseData && <HourlyWeather/>}
-				{this.props.responseData && <HourlyWeather/>}
-				{this.props.responseData && <HourlyWeather/>}
-				{this.props.responseData && <HourlyWeather/>}
-				{this.props.responseData && <HourlyWeather/>}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[0]} />}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[1]} />}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[2]} />}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[3]} />}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[4]} />}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[5]} />}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[6]} />}
+				{this.props.responseData && <HourlyWeather weather={this.props.responseData.list[7]} />}
 				{this.props.error && <h2>Please Enter a value</h2>}
 			</div>
 		);
